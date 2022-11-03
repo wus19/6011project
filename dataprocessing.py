db06519b2735fcc12c0e6c75a763040b488feb6a
wus19 <117248276+wus19@users.noreply.github.com>
2022-11-03 19:18:15 +0800

    Update dataprocessing.py

--- dataprocessing.py
+++ dataprocessing.py
@@ -4,25 +4,22 @@ import pandas as pd
 from data import  processing
 import os
 import csv
+
 path='/Users/wus19/Desktop/project/smartcity/dataset/'
-target='/Users/wus19/Desktop/project/smartcity/dataset/csv/'
+target='/Users/wus19/Desktop/project/smartcity/xlsx/'
+
 if not os.path.exists(target):
     os.makedirs(target)
 files=os.listdir(path)
 pre=processing()
-i=1986
 for file in files:
     if  '.DS_Store' not in file:
         paths=os.path.join(path,file)
         if os.path.isfile(paths):
             x=pre.filna(paths)
             s=pd.DataFrame(x)
-            target_path=os.path.join(target,'marien_{}.csv'.format(i))
+            target_path=os.path.join(target,file)
             if os.path.exists(target_path):
                 os.remove(target_path)
-            s.to_csv(target_path)
-            i+=1
+            s.to_excel(target_path,index=False)
 print('Finish!')
-
-
-
